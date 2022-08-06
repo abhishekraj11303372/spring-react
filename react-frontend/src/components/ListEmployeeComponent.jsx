624b5df93f6737aa390721e5e9c7b2eb9e5679ff
Abhishek Raj <abhishekraj1130@gmail.com>
2022-08-06 21:41:17 +0530

    list employee init with bootstrap

---
+++ react-frontend/src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+
+export default class ListEmployeeComponent extends Component {
+    constructor(props) {
+        super(props) 
+        
+        this.state = {
+            employees: []
+        }
+    }
+
+  render() {
+    return (
+      <div>
+        <h2 className='text-center'>Employees List</h2>
+        <div className='row'>
+            <table className='table table-stripped table-bordered'>
+                <thead>
+                    <tr>
+                        <th>Employee First Name</th>
+                        <th>Employee Lirst Name</th>
+                        <th>Employee Email</th>
+                        <th>Employee Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        this.state.employees.map(
+                            employees => 
+                            <tr key = {employees.id}>
+                                <td>{employees.firstName}</td>
+                                <td>{employees.lastName}</td>
+                                <td>{employees.emailId}</td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </table>
+        </div>
+      </div>
+    )
+  }
+}
