972e19f7668caabce59b243fcc7ea5b5df1ec3e5
Abhishek Raj <abhishekraj1130@gmail.com>
2022-08-07 20:49:03 +0530

    axios added witth CORS cross origin

--- spring-react/src/main/java/com/abhishekraj1130/springreact/controller/EmployeeController.java
+++ spring-react/src/main/java/com/abhishekraj1130/springreact/controller/EmployeeController.java
@@ -3,12 +3,14 @@ package com.abhishekraj1130.springreact.controller;
 import com.abhishekraj1130.springreact.model.Employee;
 import com.abhishekraj1130.springreact.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins =  "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
