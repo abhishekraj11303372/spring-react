2571d30104c3bc763ce98435e1db5e9ff6fda19c
Abhishek Raj <abhishekraj1130@gmail.com>
2022-08-08 21:19:18 +0530

    css for header and footer

--- react-frontend/src/components/HeaderComponent.jsx
+++ react-frontend/src/components/HeaderComponent.jsx
@@ -5,7 +5,7 @@ export default class HeaderComponent extends Component {
     return (
       <div>
         <header>
-            <nav className='navbar navbar-expand-md navbar-dark bg-dark'>
+            <nav className='navbar navbar-expand-md navbar-dark bg-dark p-3'>
                 <div><a href='#' className='navbar-brand'>Employee Management App</a></div>
             </nav>
         </header>
