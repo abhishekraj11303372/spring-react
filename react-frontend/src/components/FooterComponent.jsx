aaf9535528f85607c3a692d41fb6707230261c70
Abhishek Raj <abhishekraj1130@gmail.com>
2022-08-08 21:07:23 +0530

    header footer init

---
+++ react-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class FooterComponent extends Component {
+  render() {
+    return (
+        <div>
+        <footer className='footer'>
+            <span className='text-muted'>All right reserver 2022 @abhishek</span>
+        </footer>
+      </div>
+    )
+  }
+}
