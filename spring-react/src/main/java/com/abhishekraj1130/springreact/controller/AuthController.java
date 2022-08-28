51aed9c018160d573c32bc8b9bd4e355f51e553a
Abhishek Raj <abhishekraj1130@gmail.com>
2022-08-28 17:09:42 +0530

    auth controller and securtiy dependencies

--- spring-react/src/main/java/com/abhishekraj1130/springreact/controller/AuthController.java
+++ spring-react/src/main/java/com/abhishekraj1130/springreact/controller/AuthController.java
@@ -1,9 +1,11 @@
 package com.abhishekraj1130.springreact.controller;
 
 import com.abhishekraj1130.springreact.model.LoginDto;
+import com.abhishekraj1130.springreact.model.Role;
+import com.abhishekraj1130.springreact.model.SignUpDto;
+import com.abhishekraj1130.springreact.model.User;
 import com.abhishekraj1130.springreact.repository.RoleRepository;
 import com.abhishekraj1130.springreact.repository.UserRepository;
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,10 +13,13 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -33,7 +38,34 @@ public class AuthController {
                 loginDto.getEmailId(), loginDto.getPassword()));
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        return new ResponseEntity<>(body:"User Signed In Successfully", HttpStatus.OK);
+        return new ResponseEntity<>("User Signed In Successfully", HttpStatus.OK);
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody SignUpDto signUpDto){
+        //add check for username exists in DB
+        if(userRepository.existsByUsername(signUpDto.getUsername())){
+            return new ResponseEntity<>("Username is already taken", HttpStatus.BAD_REQUEST);
+        }
+
+        //add check for email exists in DB
+        if(userRepository.existsByEmail(signUpDto.getEmail())){
+            return new ResponseEntity<>("Username is already taken", HttpStatus.BAD_REQUEST);
+        }
+
+        // create user object
+        User user = new User();
+        user.setName(signUpDto.getName());
+        user.setUsername(signUpDto.getUsername());
+        user.setEmail(signUpDto.getEmail());
+        user.setPassword(passwordEncoder.encode(signUpDto.getPassword()));
+
+        Role roles = roleRepository.findByName("ROLE_ADMIN").get();
+        user.setRoles(Collections.singleton(roles));
+
+        userRepository.save(user);
+
+        return new ResponseEntity<>("User registered successfully", HttpStatus.OK);
     }
 
 }
---
+++ spring-react/src/main/java/com/abhishekraj1130/springreact/model/SignUpDto.java
@@ -0,0 +1,11 @@
+package com.abhishekraj1130.springreact.model;
+
+import lombok.Data;
+
+@Data
+public class SignUpDto {
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+}
